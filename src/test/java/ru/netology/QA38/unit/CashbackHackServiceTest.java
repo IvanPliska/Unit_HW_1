2b7a1cf6fddbf6d50dfa1b3ccb498e932b8c7d13
IvanPliska <94367970+IvanPliska@users.noreply.github.com>
2022-08-03 16:12:40 +0300

    Service by junit4

--- src/test/java/ru/netology/QA38/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/QA38/unit/CashbackHackServiceTest.java
@@ -1,48 +1,48 @@
 package ru.netology.QA38.unit;
 
-import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
-    void shouldRemainToCashbackIfPrise800() {
+    public void shouldRemainToCashbackIfPrise800() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 800;
 
         int actual = cashbackHackService.remain(amount);
         int expected = 200;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldRemainToCashbackIfPrise999() {
+   public void shouldRemainToCashbackIfPrise999() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 999;
 
         int actual = cashbackHackService.remain(amount);
         int expected = 1;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldRemainToCashbackIfPrise1000() {
+   public void shouldRemainToCashbackIfPrise1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
 
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldRemainToCashbackIfPrise1001() {
+   public void shouldRemainToCashbackIfPrise1001() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1001;
 
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
